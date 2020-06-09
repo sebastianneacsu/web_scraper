d11e247a6c89ee569c39bdc7f0acf73de677cf83
emneacsse <sebastian.neacsu@arhs-developments.com>
2020-06-09 21:00:04 +0200

    added the test class and pom.xml

---
+++ src/test/java/DownloadFileRevisited.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.assertEquals;
+
+public class DownloadFileRevisited {
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void test()
+    {
+        driver.get("https://freeqacamp.com/");
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "freeqacamp";
+        assertEquals(expectedTitle,actualTitle);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
